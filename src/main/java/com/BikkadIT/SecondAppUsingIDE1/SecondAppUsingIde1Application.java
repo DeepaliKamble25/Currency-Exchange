bf0e9c003644c86bf1d856197a7a791e8490dccb
DeepaliKamble25 <kambledeepali25@gmail.com>
2023-04-25 08:25:48 +0530

    this done by dips

---
+++ src/main/java/com/BikkadIT/SecondAppUsingIDE1/SecondAppUsingIde1Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SecondAppUsingIDE1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondAppUsingIde1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAppUsingIde1Application.class, args);
+	}
+
+}
